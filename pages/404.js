8ed6bc5bae58bc92ed5a3bd60ae8a36c1c78219d
ali <ali0995559052@gmail.com>
2021-06-02 13:01:06 +0200

    add new styles

---
+++ pages/404.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFount = () => {
+    return (
+        <div className="not-fount">
+            <h1>404</h1>
+            <h2>Ooops! That page cannot be found😥</h2>
+            <p>Redirecting to the <Link hr></Link></p>
+            
+        </div>
+    );
+}
+
+export default NotFount;
